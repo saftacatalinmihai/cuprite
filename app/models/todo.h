7a58807c9251ac2b70380308efb9dd2ddfaba85e
Catalin Mihai Safta <catalin-mihai.safta@ing.com>
2025-07-21 23:04:43 +0300

    todo mvc wip

---
+++ app/models/todo.h
@@ -0,0 +1,11 @@
+#ifndef TODO_H
+#define TODO_H
+
+typedef struct {
+    int id;
+    char* text;
+    int completed; // boolean: 0 false, 1 true;
+} Todo;
+
+#endif
+
--- app/routes.c
+++ app/routes.c
@@ -1,7 +1,9 @@
 #include "router.h"
 #include "controllers/products_controller.h"
+#include "controllers/todos_controller.h"
 
 void initialize_routes(void) {
+    // PRODUCTS
     route_get("/products", products_index);
 
     route_get("/products/new", products_new);
@@ -14,4 +16,18 @@ void initialize_routes(void) {
     route_put("/products/:id", products_update);
 
     route_delete("/products/:id", products_destroy);
+
+    // TODOS
+    route_get("/todos", todos_index);
+
+    route_get("/todos/new", todos_new);
+    route_post("/todos", todos_create);
+
+    route_get("/todos/:id", todos_show);
+
+    route_get("/todos/:id/edit", todos_edit);
+    route_patch("/todos/:id", todos_update);
+    route_put("/todos/:id", todos_update);
+
+    route_delete("/todos/:id", todos_destroy);
 }
